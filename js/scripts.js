40232ec50e92bcdb847d0713b24686b268b01daa
savespace <savespace@outlook.com>
2017-07-25 17:41:34 +0200

    Pierwszy skrypt

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+var name = prompt('Wpisz swoje imie');
+alert('Witaj, ' + name);
+alert('Strona na której się znajdujesz została napisana w celach testowych');
+console.log('Witaj, ' + name);
